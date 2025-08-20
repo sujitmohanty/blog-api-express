c73b549d3da5fd27794f22a80cc75eec2e4d26b3
Sujit Mohanty <sujit.mty@outlook.com>
2025-08-20 11:22:56 +0200

    chore: install and configure more dependencies

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Import dependencies
+ */
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+};
+
+export default config;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,73 @@
  * Import Dependencies
  */
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import helmet from 'helmet';
+
+/**
+ * Import custom modules
+ */
+import config from '@/config';
+import limiter from '@/lib/express_rate_limit';
+
+/**
+ * Types
+ */
+import type { CorsOptions } from 'cors';
 
 /**
  * Initialise Express Application
  */
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`Server running: http://localhost:3000`);
+// Configure CORS options
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (config.NODE_ENV === 'development' || !origin) {
+      callback(null, true);
+    } else {
+      // Reject requests from non-whitelisted origins
+      callback(
+        new Error(`CORS error: ${origin} is not allowed by CORS`),
+        false,
+      );
+    }
+  },
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+// Enable JSON request body parsing
+app.use(express.json());
+
+// Enable URL-encoded request body parsing with extended mode
+// `extended: true` allows rich objects and arrays via querystring library
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+// Enable response compression to reduce payload size and improve performance
+app.use(
+  compression({
+    threshold: 1024, // Only compress responses larger than 1KB
+  }),
+);
+
+// Use Helmet to enhance security by setting various HTTP headers
+app.use(helmet());
+
+// Apply rate limiting middleware to prevent excessive requests and enhance security
+app.use(limiter);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server running: http://localhost:${config.PORT}`);
 });
